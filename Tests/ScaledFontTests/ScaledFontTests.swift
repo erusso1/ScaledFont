a438ffe5961f6fac1297b82eaa040eb83512d5d2
Ephraim Russo <ephraim.s.russo@gmail.com>
2019-12-22 22:12:38 -0800

    Initial commit

--- Tests/ScaledFontTests/ScaledFontTests.swift
+++ Tests/ScaledFontTests/ScaledFontTests.swift
@@ -6,7 +6,7 @@ final class ScaledFontTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(ScaledFont().text, "Hello, World!")
+        
     }
 
     static var allTests = [
